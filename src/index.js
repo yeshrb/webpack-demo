fcd4153e12676da965420227026f5fc62b84a831
lsm1002 <lsm1002@126.com>
2017-11-06 23:54:53 +0800

    Loading Images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
     return element;
 }
 
