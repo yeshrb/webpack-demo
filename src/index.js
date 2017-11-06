a5b417a2a73a642c476033d811e9e2a785d81593
lsm1002 <lsm1002@126.com>
2017-11-07 07:35:07 +0800

    output management

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  console.log(Data);
-    return element;
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
+  return element;
 }
 
 document.body.appendChild(component());
